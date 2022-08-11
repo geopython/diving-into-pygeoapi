ed492b8a5bfb48efb16f92ddad534d8b43a9d9ff
Just van den Broecke <just@justobjects.nl>
2022-08-11 16:33:11 +0200

    #57 Elastic Cleanup- step 1 - built-in data working

--- workshop/exercises/elastic/ES/docker-entrypoint.sh
+++ workshop/exercises/elastic/ES/docker-entrypoint.sh
@@ -32,5 +32,6 @@
 
 # wait for Elasticsearch to start, then run the setup script to
 # create and configure the index.
-exec /usr/share/elasticsearch/bin/wait-for-it.sh localhost:9200 &
+# exec /usr/share/elasticsearch/bin/wait-for-it.sh localhost:9200 &
+exec /usr/share/elasticsearch/bin/wait-for-it.sh localhost:9200 -- /add_data.sh &
 exec $@
